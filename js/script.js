f31995ea09d217c13beddfd52a0f200571ec1d3f
Jakub Stelmasiak <stelmon3@gmail.com>
2023-10-26 13:19:37 +0200

    playerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let computerMove = 'kamień';
 let playerMove = 'papier'
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' playerMove ', to wygrywasz!');
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
 
 
