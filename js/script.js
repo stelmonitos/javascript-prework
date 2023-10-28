a6b64e574aa5dce38114cd958726605715fc8e01
Jakub Stelmasiak <stelmon3@gmail.com>
2023-10-28 19:55:16 +0200

    Final game without buttons

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let computerMove = getMoveName(randomNumber);
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let playerMove = getMoveName(parseInt(playerInput)); // Parse player input as an integer
+let playerMove = getMoveName(playerInput);
 
 console.log('Gracz wpisał: ' + playerInput);
 printMessage('Twój ruch to: ' + playerMove);
@@ -46,39 +46,3 @@ function displayResult(argComputerMove, argPlayerMove) {
 }
 
 displayResult(computerMove, playerMove);
-
-
-
-
-
-/*
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber == 2){
-    computerMove = 'papier';
-  }
-else if(randomNumber == 3){
-    computerMove = 'nożyce';
-  }
-*/
-
-/*
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-else if(playerInput == '2'){
-    playerMove = 'papier';
-  }
-else if(playerInput == '3'){
-    playerMove = 'nożyce';
-  }
-  else printMessage
-
-printMessage('Twój ruch to: ' + playerMove);
-*/
-/*
-
-}
-*/
